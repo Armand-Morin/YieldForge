2e53220093317a3126967bd0ca0baca5dc077a1e
Armand Morin <72650161+Armand-Morin@users.noreply.github.com>
2023-05-05 09:35:20 -0700

    j

--- migrations/2_deploy_contracts_Staking.js
+++
@@ -1,5 +0,0 @@
-var chatgptStaking = artifacts.require("chatgptStaking.sol");
-
-module.exports = function (deployer) {
-  deployer.deploy(chatgptStaking, "0x06502a4e8891c00144cb7136da25a2b873266f8a28d57f53d69e7208f051c25f");
-};
